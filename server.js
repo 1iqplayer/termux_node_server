74d205493e4071b08b4e2c789bc02808fdde96e9
1iqplayer <66035695+1iqplayer@users.noreply.github.com>
2022-03-14 19:55:19 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
 http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    let q = url.parse(req.url, true).query;
-    var txt = q.imie + ' ' + q.ksywa;
-    res.end(txt);
-}).listen(8080);
+    if(req.url == '/style.css'){
+        fs.readFile('style.css', function(err, data){
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.write(data);
+            return res.end();
+        });
+    }else{
+        fs.readFile('index.html', function(err, data){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            if (err) throw err;
+            res.write(data);
+            return res.end();
+        });
+    }
+}).listen(8081);
