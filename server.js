337205226ebf693e3c29a57c1007b37f2c7a1ad8
1iqplayer <66035695+1iqplayer@users.noreply.github.com>
2022-03-23 20:27:48 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -5,17 +5,21 @@ var fs = require('fs');
 var timecheckCount = 0;
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
+    var quer = q.query;
+
     if (q.pathname == '/time'){
         let ts = Date.now();
         res.writeHead(200, {'content-type': 'text/plain'});
-        let mess = 
         res.end((Math.floor(ts/1000)).toString());
         return;
     }
+
     if (q.pathname == '/check'){
         let ts = new Date();
-        let data = (timecheckCount++).toString() + ". checked at: " +ts.getHours().toString() + ":" + ts.getMinutes().toString() +":" +ts.getSeconds().toString();
-        fs.appendFile('readings/timestamps.txt', data +'\n', function (err) {
+        let when = (timecheckCount++).toString() + ". checked at: " +ts.getHours().toString() + ":" + ts.getMinutes().toString() +":" +ts.getSeconds().toString();
+        let readings = "  Pressure: " + quer.pressure + "  Temp: " + quer.temp;
+        
+        fs.appendFile('readings/timestamps.txt', when + readings + '\n', function (err) {
             if (err) {throw err}else{
                 res.writeHead(200, {'content-type': 'text/plain'});
                 res.end('Checked succesfully!');
@@ -23,6 +27,7 @@ http.createServer(function (req, res) {
             });
         return;
     }
+
     res.writeHead(200, {'content-type': 'text/plain'});
     res.end('enter /time or /check');
   }).listen(8080);
